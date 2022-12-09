fdb7e154d1e7fdcb82fe0d055adf5deca7f73a17
alezalez8 <5446943@ukr.net>
2022-12-09 22:12:23 +0200

    add

---
+++ 01/js/script01.js
@@ -0,0 +1,17 @@
+let name1 = 'Aleks';
+const PSSWRD = 'PASS';
+//alert(name1);
+console.log('My name',name1);
+console.log(PSSWRD, 1122);
+ 
+document.write(PSSWRD);
+document.write("<br/>");
+document.write(name1);
+
+let stringNumber = '2022';
+let testNumber = 1;
+let nextYear = stringNumber + testNumber;
+console.log(nextYear);
+console.log(stringNumber - testNumber);
+console.log(stringNumber * testNumber);
+console.log(stringNumber / testNumber);

d60393705c9e9a6dd76f68410248016844947fb9
alezalez8 <5446943@ukr.net>
2022-12-13 23:11:18 +0200

    3

--- 01/js/script01.js
+++ 01/js/script01.js
@@ -1,3 +1,5 @@
+
+//  1 === отримати 2 цифрових значення і вивести результат математичнихх операцій в console.===
 // let numberOne = prompt('Input first number');
 // let numberTwo = prompt('Input second number');
 // let result = numberOne + numberTwo;
@@ -9,21 +11,27 @@
 // result = numberOne / numberTwo;
 // console.log('Divide is', result);
 
+//2  === oтримати 2 текстових значення і вивсести за допомогою document .write  "User name: FirstVar  User last name SecondVar " ( firstVar, secondVar значення які отримали ) 
 // let firstVar = prompt('Input user name');
 // let secondVar = prompt('Input user lastname');
 // document.write('"User name: ', firstVar, '  User last name: ', secondVar, '"');
 
+// 3  === отримати текстове  значення і виконати перевіку: якщо  uk повернути true, якщо ru  повернути false
+// let langSelect = prompt('Enter ru or uk');
+// if(langSelect =='uk'){
+//    document.write('true');
+//    console.log('true');
+// } else if(langSelect == 'ru') {
+//    document.write('false');
+//    console.log('false');
+// } else{
+//    document.write('You not enter ru either uk');
+//    console.log('You not enter ru either uk');
+// }
 
 
-let langSelect = prompt('Enter ru or uk');
-if(langSelect =='uk'){
-   document.write('true');
-   console.log('true');
-} else if(langSelect == 'ru') {
-   document.write('false');
-} else{
-   document.write('You not enter ru either uk');
-}
+
+
 
  
 

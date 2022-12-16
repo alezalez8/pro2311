44a22acd40c49834fc4d032820eb15337f1f943b
alezalez8 <5446943@ukr.net>
2022-12-16 19:41:31 +0200

    22

--- 02/js/style.js
+++ 02/js/style.js
@@ -1,7 +1,7 @@
 //let yu = prompt('enter data');
 //yu = parseFloat(yu);
 //console.log('You enter ', yu);
-let myword = prompt('my word');
-console.log(myword);
-let myword1 = myword.toUpperCase();
-console.log(myword1);
+////let myword = prompt('my word');
+//console.log(myword);
+//let myword1 = myword.toUpperCase();
+//console.log(myword1);

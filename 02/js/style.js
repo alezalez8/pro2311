8feebc9e2dd996eb95c809e29bc8a7bd5b11db9a
alezalez8 <5446943@ukr.net>
2022-12-17 00:35:24 +0200

    22

--- 02/js/style.js
+++ 02/js/style.js
@@ -39,5 +39,32 @@
 //    console.log('Arithmetic progression is ', arithmeticPp);
 // } else console.log('First number has to be less then number two');
 
+// loop decrease
+// let firstNumberLoop = parseInt(prompt('Enter first number'));
+// let secondNumberLoop = parseInt(prompt('Enter second number'));
+// for(secondNumberLoop; secondNumberLoop >= firstNumberLoop; secondNumberLoop--){
+//    console.log('i = ', secondNumberLoop);
+// }
+
+// drow figure
+// let width = parseInt(prompt('Input width'));
+// let heigth = parseInt(prompt('Input heigth'));
+// let symbol = '';
+// for(let j = 0; j < width; j++){   
+//    symbol += '*';
+// }
+// for(let i = 0; i < heigth; i++){
+//    console.log(symbol);
+// }
+
+// authorization
+// let loginWord = prompt('Input your login');
+// let password = prompt('Input your password');
+// if(loginWord == '12345' && password == '12345') {
+//    console.log('Вітаємо в системі');
+// } else console.log ('Пароль логін не вірні');
+
+
+
 
 

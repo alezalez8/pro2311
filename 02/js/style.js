741e69567233bfde3dd65485019e46840898f208
alezalez8 <5446943@ukr.net>
2022-12-16 23:24:06 +0200

    22

--- 02/js/style.js
+++ 02/js/style.js
@@ -1,7 +1,43 @@
-//let yu = prompt('enter data');
-//yu = parseFloat(yu);
-//console.log('You enter ', yu);
-////let myword = prompt('my word');
-//console.log(myword);
-//let myword1 = myword.toUpperCase();
-//console.log(myword1);
+//calculator
+// let a = parseInt(prompt('Enter first number'));
+// let b = parseInt(prompt('Enter second number'));
+// let result = 0;
+// let action = prompt('Enter operation, for example + - / *');
+// switch(action){
+//    case '+': result = a + b;
+//    break;
+//    case '-': result = a - b;
+//    break;
+//    case '*': result = a * b;
+//    break;
+//    case '/': {
+//       if(b == 0){
+//          alert('Divide by zero is not possible');        
+//          result = 'not calculate';
+//          break;
+//       } else{
+//          result = a / b;
+//          break;
+//       }
+//    }    
+// }
+// console.log('Result of operation is ', result);
+
+// even number
+// for(let i = 0; i <=20; i++ ){
+//    if(!(i & 1) == 1) console.log(i);  
+// }
+
+// loop for two number
+// let firstNumber = parseInt(prompt('Enter first number'));
+// let secondNumber = parseInt(prompt('Enter second number'));
+// let arithmeticPp = 0;
+// if(firstNumber < secondNumber){
+//    for(firstNumber; firstNumber <=secondNumber; firstNumber++){
+//       arithmeticPp +=firstNumber;
+//    }
+//    console.log('Arithmetic progression is ', arithmeticPp);
+// } else console.log('First number has to be less then number two');
+
+
+

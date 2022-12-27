9af0e5c87e430ec64eee40e2e7fc6047cc12ccfe
unknown <alezalez8>
2022-12-27 19:09:06 +0200

    5111

--- 05/js/myjs.js
+++ 05/js/myjs.js
@@ -1,1 +1,18 @@
-console.log('helloy');
+let arr = [1, 3, 6, 3, 22, 4, 12];
+let arr2 = [
+    {
+        name: 'qwert',
+        a: 23,
+        b: 24
+    },
+    {
+        name: 'qazwsxr',
+        a: 25,
+        b: 28
+    }
+]
+function mmm(arr) {
+    arr.forEach(element => {
+        console.log(element);
+    });
+}

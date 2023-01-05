0a60decf0ef11802be64b38fd18f8201afb95bc3
alezalez8 <5446943@ukr.net>
2023-01-06 01:03:52 +0200

    6_1

--- 06/js/myjs.js
+++ 06/js/myjs.js
@@ -1,24 +1,26 @@
 // ===== task 1 =====
 
-// function uknownArguments(...myArgs) {
-//   let totalSumm = 0;
-//   if (myArgs.length > 2) {
-//     for (let numb of myArgs) {
-//       if (typeof numb == "number" && !isNaN(numb)) {
-//         totalSumm += numb;
-//       }
-//     }
-//     return totalSumm;
-//   } else {
-//     console.log("There are less then two or more arguments");
-//     return myArgs;
-//   }
-// }
+function uknownArguments(...myArgs) {
+  let totalSumm = 0;
+  if (myArgs.length > 2) {
+    for (let numb of myArgs) {
+      if (typeof numb == "number" && !isNaN(numb)) {
+        totalSumm += numb;
+      }
+    }
+    return totalSumm;
+  } else {
+    console.log("There are less then two or more arguments");
+    return myArgs;
+  }
+}
 
-// // ===== test 1 =====
-// console.log(uknownArguments(1, 4, 5));
-// console.log(uknownArguments(1, "as", 7, "qwert", 12));
-// console.log(uknownArguments(1));
+// ===== test 1 =====
+console.log(uknownArguments(1, 4, 5));
+console.log(uknownArguments(1, "as", 7, "qwert", 12));
+console.log(uknownArguments(1));
+
+console.log("=============================================================");
 
 // ===== task 2 - 3 =====
 
@@ -53,8 +55,47 @@ function getAge(dateBirthday, monthBirthday, yearBirthday, happyBirthday) {
   console.log("Your age is ", userAge);
   happyBirthday(dateBirthday, monthBirthday);
 }
+
 // ===== test 2-3 =====
 getAge(05, 01, 1984, happyBirthday);
 getAge(06, 01, 1995, happyBirthday);
 
+console.log("=============================================================");
+
 // ===== task 4 =====
+
+function getCentury(userYear) {
+  let year = parseInt(userYear);
+  return Math.ceil(year / 100);
+}
+// ===== test 4 =====
+console.log(getCentury(1810));
+console.log(getCentury(1700));
+console.log(getCentury(1601));
+console.log(getCentury(2000));
+console.log(getCentury(1285));
+
+console.log("=============================================================");
+
+// ===== task 5 =====
+function getAmountDays() {
+  let presentDate = new Date();
+  console.log("Now is ", presentDate.toDateString());
+  let arrayDays = [];
+
+  let currentMonth = new Date(
+    presentDate.getFullYear(),
+    presentDate.getMonth(),
+    0
+  ).getDate();
+  presentDate.setMonth(presentDate.getMonth() + 2);
+  let nextMonth = new Date(
+    presentDate.getFullYear(),
+    presentDate.getMonth(),
+    0
+  ).getDate();
+  return (arrayDays = [currentMonth, nextMonth]);
+}
+
+// ===== test 5 =====
+console.log(getAmountDays());

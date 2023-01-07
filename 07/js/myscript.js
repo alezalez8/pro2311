fb8e284c8ad0eedd8f9738066815f15922016f00
alezalez8 <5446943@ukr.net>
2023-01-07 16:03:25 +0200

    7_1

--- 07/js/myscript.js
+++ 07/js/myscript.js
@@ -16,4 +16,27 @@ myfooter.innerHTML = "Change footer";
 
 // ===== task 2 =====
 
+let myLi = document.querySelector("li");
+console.log(myLi.parentElement);
 
+// ===== task 3 - 5 =====
+
+class UserMovie {
+  constructor(year, genre, title, showAmount) {
+    this.year = year;
+    this.genre = genre;
+    this.title = title;
+    this.showAmount = showAmount;
+  }
+  rateMovie = function () {
+    return showAmount / (new Date().getFullYear - this.year);
+  };
+}
+
+let filmLibrary = [
+  new UserMovie(1972, "horor", "QQQ", 45),
+  new UserMovie(1992, "horor", "QQQ", 45),
+  new UserMovie(2020, "horor", "QQQ", 45),
+  new UserMovie(2001, "horor", "QQQ", 45),
+  new UserMovie(1995, "horor", "QQQ", 45),
+];

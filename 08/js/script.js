b0c3729e0f5f5216a7ee8e3b3c8432825d4914d5
alezalez8 <5446943@ukr.net>
2023-01-13 15:01:00 +0200

    8_1

--- 08/js/script.js
+++ 08/js/script.js
@@ -16,6 +16,16 @@ let newElem = document.createElement('p');
 newElem.innerHTML = 'PPPP';
 headers.appendChild(newElem);
 
+let footer = document.querySelector('footer');
+let div = document.createElement('div');
+div.id = 'js';
+div.className = 'js-class';
+div.innerText = ' Change div';
+footer.appendChild(div);
+
+let list = document.querySelector('.list');
+let li = 
+
 
 
 

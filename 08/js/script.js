c63f6aaa52b5df39d5398030c682fdfb4745c351
alezalez8 <5446943@ukr.net>
2023-01-13 22:49:10 +0200

    8_1

--- 08/js/script.js
+++ 08/js/script.js
@@ -1,65 +1,76 @@
 window.onload = () => {
-//   let headers = document.querySelector("header");
+  //   let headers = document.querySelector("header");
 
-//    console.log(headers, "Headers");
-// //   headers.innerHtml = "Helloy";
-// //   headers[1].innerHTML = "hhhhh";
-// //   headers[2].innerHTML = `<div>Helloooy</div>`;
+  //    console.log(headers, "Headers");
+  // //   headers.innerHtml = "Helloy";
+  // //   headers[1].innerHTML = "hhhhh";
+  // //   headers[2].innerHTML = `<div>Helloooy</div>`;
 
-//   let [...myBody] = document.querySelectorAll('body');
-//   console.log(myBody);
+  //   let [...myBody] = document.querySelectorAll('body');
+  //   console.log(myBody);
 
+  // let newElem = document.createElement('p');
+  // newElem.innerHTML = 'PPPP';
+  // headers.appendChild(newElem);
 
-  
+  // let footer = document.querySelector('footer');
+  // // let div = document.createElement('div');
+  // // div.id = 'js';
+  // // div.className = 'js-class';
+  // // div.innerText = ' Change div';
+  // // footer.appendChild(div);
 
-// let newElem = document.createElement('p');
-// newElem.innerHTML = 'PPPP';
-// headers.appendChild(newElem);
+  // // let list = document.querySelector('.list');
+  // //   let li = document.createElement('li');
+  // //   li.innerText = 'first item';
+  // //   list.appendChild(li);
 
-// let footer = document.querySelector('footer');
-// // let div = document.createElement('div');
-// // div.id = 'js';
-// // div.className = 'js-class';
-// // div.innerText = ' Change div';
-// // footer.appendChild(div);
+  // //   function myList(numbRow) {
+  // //     for (let i = 0; i < numbRow; i++) {
+  // //     let li = document.createElement('li');
+  // //     li.textContent = 'first item';
+  // //     list.appendChild(li);
+  // //   }
+  // //   }
 
-// // let list = document.querySelector('.list');
-// //   let li = document.createElement('li');
-// //   li.innerText = 'first item';
-// //   list.appendChild(li);
+  // //   function myMenuList(name) {
+  // //     let li = document.createElement('li');
+  // //     li.innerText = name;
+  // //     list.appendChild(li);
+  // //   }
 
-// //   function myList(numbRow) {
-// //     for (let i = 0; i < numbRow; i++) { 
-// //     let li = document.createElement('li');
-// //     li.textContent = 'first item';
-// //     list.appendChild(li);
-// //   }
-// //   }
+  //   // myMenuList('text');
+  //   // myMenuList('htll');
+  //   // myMenuList('About');
 
-// //   function myMenuList(name) {
-// //     let li = document.createElement('li');
-// //     li.innerText = name;
-// //     list.appendChild(li);
-// //   }
-
-//   // myMenuList('text');
-//   // myMenuList('htll');
-//   // myMenuList('About');
-
-//   let lastItem = list.lastElementChild;
-//   console.log(lastItem);
-//   list.removeChild(lastItem);
-  let firstLink = document.querySelector('#first');
-  let secondLink = document.querySelector('#second');
-  let thirdLink = document.querySelector('#third');
+  //   let lastItem = list.lastElementChild;
+  //   console.log(lastItem);
+  //   list.removeChild(lastItem);
+  let firstLink = document.querySelector("#first");
+  let secondLink = document.querySelector("#second");
+  let thirdLink = document.querySelector("#third");
   console.log(thirdLink);
 
-  firstLink.setAttribute('href', 'www.google.com.ua');
-  firstLink.textContent = 'My search';
-  secondLink.removeAttribute('href');
-  let tL = thirdLink.getAttribute('href');
+  firstLink.setAttribute("href", "www.google.com.ua");
+  firstLink.textContent = "My search";
+  secondLink.removeAttribute("href");
+  let tL = thirdLink.getAttribute("href");
   console.log(tL);
 
+  let menuItems = ["Home  ", "News  ", "Posts"];
+  let nav = document.querySelector("nav");
+  let fragment = new DocumentFragment();
+  menuItems.forEach((item) => {
+    let link = document.createElement("a");
+    link.innerText = item;
+    //link.setAttribute("href", "cccc");
+    fragment.appendChild(link);
+  });
 
+  nav.appendChild(fragment);
 
+  let title = document.querySelector('h1');
+  let textP = document.createElement('p');
+  textP.innerText = 'Hello in P';
+  title.after(textP);
 };

8e3638eedccc008928aaf5192e6288d3989180fc
alezalez8 <5446943@ukr.net>
2023-01-19 20:28:14 +0200

    8_1

--- 08/js/script.js
+++ 08/js/script.js
@@ -79,9 +79,11 @@ window.onload = () => {
     let fragmentOfImages = new DocumentFragment();
     arrayOfImages.forEach((images) => {
       let impLink = document.createElement("a");
-      let img = document.createElement("img");
       impLink.setAttribute("href", images.href);
-      //impLink.innerText = 'href';
+      let img = document.createElement("img");
+      img.src = images.imgPath;     
+      img.alt = images.alt;
+      img.width = 200;
       impLink.appendChild(img);
       fragmentOfImages.appendChild(impLink);
     });
@@ -93,14 +95,4 @@ window.onload = () => {
   // ===== end task 2 =====
 };
 
-// women
-// https://pixabay.com/ru/photos/%d0%b6%d0%b5%d0%bd%d1%89%d0%b8%d0%bd%d0%b0-%d1%81%d0%b8%d0%bb%d1%83%d1%8d%d1%82-%d0%b7%d0%b0%d1%85%d0%be%d0%b4-%d1%81%d0%be%d0%bb%d0%bd%d1%86%d0%b0-%d0%bf%d0%bb%d1%8f%d0%b6-570883/
 
-// sunset
-// https://pixabay.com/ru/photos/%d0%bc%d0%be%d1%80%d0%b5-%d0%b7%d0%b0%d1%85%d0%be%d0%b4-%d1%81%d0%be%d0%bb%d0%bd%d1%86%d0%b0-%d0%bb%d0%be%d0%b4%d0%ba%d0%b0-%d1%81%d1%83%d0%bc%d0%b5%d1%80%d0%ba%d0%b8-164989/
-
-// ship
-// https://pixabay.com/ru/photos/%d0%b7%d0%b0%d1%85%d0%be%d0%b4-%d1%81%d0%be%d0%bb%d0%bd%d1%86%d0%b0-%d1%81%d1%83%d0%b4%d0%bd%d0%be-%d0%bf%d0%b0%d1%80%d1%83%d1%81%d0%b0-%d0%bb%d0%be%d0%b4%d0%ba%d0%b0-675847/
-
-// flowers
-//https://pixabay.com/ru/photos/%d0%bc%d0%b0%d0%ba%d0%b8-%d0%bf%d0%be%d0%bb%d0%b5-%d0%b7%d0%b0%d1%85%d0%be%d0%b4-%d1%81%d0%be%d0%bb%d0%bd%d1%86%d0%b0-%d1%81%d1%83%d0%bc%d0%b5%d1%80%d0%ba%d0%b8-174276/

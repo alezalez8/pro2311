9ebd31fd1ffbde950dd56514ad1ff3a5e21a2628
alezalez8 <5446943@ukr.net>
2023-01-21 16:36:30 +0200

    9_1

--- 09/js/script.js
+++ 09/js/script.js
@@ -4,14 +4,45 @@ window.onload = () => {
   let width = container.offsetWidth;
   console.log(width);
   console.log(container.offsetHeight);
-  let domRext =  container.getBoundingClientRect();
+  let domRext = container.getBoundingClientRect();
   console.log(domRext);
   let widthC = container.clientWidth;
   console.log(widthC);
   let contStyle = getComputedStyle(container);
-  console.log(contStyle);
+  //console.log(contStyle);
   let marginLeft = parseInt(contStyle.marginLeft);
   console.log(marginLeft);
 
-};
+  // let btn = document.querySelector("#alertBtn");
 
+  // function displayAlert() {
+  //   alert("Click button");
+  // }
+  // btn.addEventListener("click", displayAlert);
+
+  // let btnRemove = document.querySelector("#remove");
+
+ 
+
+  // btnRemove.addEventListener('click', ()=>{
+  //   btn.removeEventListener('click', displayAlert);
+  //   alert("Remove done");
+  // });
+
+let h1 = document.querySelector('h1');
+h1.addEventListener('click', function(){
+  console.log(this);
+  this.textContent = 'Helloy';
+})
+
+let [...pArr] = document.querySelectorAll('.div p');
+console.log(pArr);
+for(let i = 0; i < pArr.length; i++){
+  pArr[i].addEventListener('click', function(){
+    this.style.background = 'red';
+    this.textContent = `My index ${i}`;
+  })
+}
+
+
+};

26cdeaec4f73845024492ecdba44565e84c98357
unknown <alezalez8>
2023-01-24 20:17:10 +0200

    9_1

--- 09/js/script.js
+++ 09/js/script.js
@@ -1,19 +1,20 @@
 window.onload = () => {
   // ===== task 1 =====
   let offset = 0;
+  let absolOffset = 384;
   const sliderline = document.querySelector(".slider-line");
   document.querySelector(".slider-next").addEventListener("click", function () {
-    offset += 300;
-    if (offset > 1832) {
+    offset += absolOffset;
+    if (offset > 2688) {
       offset = 0;
     }
     sliderline.style.left = -offset + "px";
   });
 
   document.querySelector(".slider-prev").addEventListener("click", function () {
-    offset -= 300;
+    offset -= absolOffset;
     if (offset < 0) {
-      offset = 1832;
+      offset = 2688;
     }
     sliderline.style.left = -offset + "px";
   });

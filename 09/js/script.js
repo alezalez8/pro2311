cc434e12d03c14ca828c7aea9bbfd232c2f1c916
unknown <alezalez8>
2023-01-24 20:26:24 +0200

    9_1

--- 09/js/script.js
+++ 09/js/script.js
@@ -3,6 +3,13 @@ window.onload = () => {
   let offset = 0;
   let absolOffset = 384;
   const sliderline = document.querySelector(".slider-line");
+  let [...imgArray] = document.querySelectorAll('img');
+  console.log(imgArray.toString());
+  
+
+// ===== end task 1 =====
+// ===== task 2 =====
+  
   document.querySelector(".slider-next").addEventListener("click", function () {
     offset += absolOffset;
     if (offset > 2688) {
@@ -18,4 +25,6 @@ window.onload = () => {
     }
     sliderline.style.left = -offset + "px";
   });
+
+// ===== end task 2 =====
 };
